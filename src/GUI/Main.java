8250d889930cd848a8fd1c02049b84443031f04d
DennisPC-bit <dnisp@live.dk>
2021-02-08 10:13:58 +0100

    Changed names, chnaged up the way fmlloader is defined

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +8,23 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Stage primaryStage;
+
+    private ViewController viewController;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+        Parent root = loader.load();
+        this.viewController=loader.getController();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+    }
+
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
 
